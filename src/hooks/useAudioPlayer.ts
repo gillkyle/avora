ae052aa8f1fe702e8f7f4fcb3453bca5c366c643
Kyle Gill <kylerobertgill@gmail.com>
2025-01-29 16:37:02 -0700

    first attempt at howler player?...

--- src/hooks/useAudioPlayer.ts
+++ src/hooks/useAudioPlayer.ts
@@ -1,12 +1,45 @@
+import { Howl } from "howler";
 import * as React from "react";
 
 export function useAudioPlayer(soundUrl: string) {
   const [isPlaying, setIsPlaying] = React.useState(false);
   const [volume, setVolume] = React.useState(0.5);
 
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
+  const sound = React.useRef<Howl>(
+    new Howl({
+      src: [soundUrl],
+      volume: volume,
+      loop: true,
+    })
+  );
+
+  function play() {
+    sound.current.play();
+    setIsPlaying(true);
   }
+
+  function pause() {
+    sound.current.pause();
+    setIsPlaying(false);
+  }
+
+  function togglePlay() {
+    if (isPlaying) {
+      pause();
+    } else {
+      play();
+    }
+  }
+
+  React.useEffect(() => {
+    sound.current.volume(volume);
+  }, [volume]);
+
+  React.useEffect(() => {
+    return () => {
+      sound.current.unload();
+    };
+  }, []);
 
   return { isPlaying, volume, setVolume, togglePlay };
 }
